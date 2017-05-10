dbe8211a51e7d209b87a266976bc30202bb3b6a7
Amer Kodzaga <akodzaga1@etf.unsa.ba>
2017-05-10 20:35:46 +0200

    Prva verzija Frontend

---
+++ program/app/router.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('about');
+  this.route('pretraga');
+  this.route('kategorije');
+  this.route('profil');
+  this.route('faq');
+  this.route('login');
+  this.route('iznajmljivanje');
+});
+
+export default Router;
