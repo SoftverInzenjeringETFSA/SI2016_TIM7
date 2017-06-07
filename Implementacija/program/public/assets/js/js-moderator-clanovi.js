dbb39868d5fc0c5738f6adda0b19b70e08f7bdbe
Almir Hamza <ahamza1@etf.unsa.ba>
2017-06-07 23:59:44 +0200

    resolved issue #3

--- Implementacija/program/public/assets/js/js-moderator-clanovi.js
+++ Implementacija/program/public/assets/js/js-moderator-clanovi.js
@@ -7,4 +7,27 @@ $(document).ready(function () {
 	        element.closest('tr').fadeOut(300);
 	    });
 	});
+
+	$('#dodaj-novog-clana').on('click', function () {
+
+		$.post('http://localhost:8080/administracija/clanovi/dodaj', { 
+			broj_clanske_karte: $('#broj_clanske_karte').val(), 
+			datum_rodjenja: $('#datum_rodjenja').val(), 
+			adresa: $('#adresa').val(),
+			mjesto_stanovanja: $('#mjesto_stanovanja').val(), 
+			broj_telefona: $('#broj_telefona').val(), 
+			ustanova: $('#ustanova').val(), 
+			email: $('#email').val(), 
+			korisnicko_ime: $('#korisnicko-ime').val() })
+		.done(function () {
+			alert('Novi član uspješno dodan!');
+			location.reload();
+		})
+		.fail(function (xhr, textStatus, errorthrown) {
+			if(xhr.status == 406)
+				alert('Osoba sa tim korisničkim imenom je već član biblioteke!');
+			else
+				alert('Došlo je do greške, statusni kod greške: ' + xhr.status);
+		});
+	});
 });
